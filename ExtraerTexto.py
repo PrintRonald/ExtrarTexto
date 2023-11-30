5a083e11c31dbd0f855fcb6c447a736ccea66e6c
PrintRonald <madariaga492@gmail.com>
2023-11-30 00:53:52 -0300

    Comit2
    Se realiza algunas modificaciones al script

---
+++ ExtraerTexto.py
@@ -0,0 +1,14 @@
+import pytesseract
+from PIL import Image
+
+# Especifica la ruta de la imagen
+ruta_imagen = r'C:\Users\madar\Documents\GitHub\ExtrarTexto\Factura.jpeg'
+
+# Carga la imagen usando PIL (Python Imaging Library)
+imagen = Image.open(ruta_imagen)
+
+# Utiliza pytesseract para extraer el texto de la imagen
+texto_extraido = pytesseract.image_to_string(imagen)
+
+# Imprime el texto extraído
+print(texto_extraido)
