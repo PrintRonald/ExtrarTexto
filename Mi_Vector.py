ea2845df4db15293bc3c7c4242aacd2cb4ae5890
PrintRonald <madariaga492@gmail.com>
2023-12-25 22:44:54 -0300

    Tarea Udemy1
    Se realizan unos ejercicios del curso de python de la A a la Z

--- Mi_Vector.py
+++ Mi_Vector.py
@@ -1,11 +1,20 @@
 from vectors import *
 
-Mi_vector = Vector2D(2,3)
+#Mi_vector = Vector2D(2,3)
 
-print(Mi_vector)
+#print(Mi_vector)
 
-print(Mi_vector.module)
+#print(Mi_vector.module)
 
-print(Mi_vector.scalar_prod(3))
+#print(Mi_vector.scalar_prod(3))
+
+v1 = Vector2D(2,3)
+v2 = Vector2D(4,6)
+
+#print(Vector2D.distance(v1, v2))
 
 
+v3 = Vector3D(1,2,3)
+v4 = Vector3D(2,4,6)
+#print(v3.scalar_prod(2))
+print(Vector3D.substract(v3, v4))
--- vectors.py
+++ vectors.py
@@ -18,3 +18,72 @@ class Vector2D():
     
     def __str__(self):
         return "({},{})".format(self.x, self.y)
+    
+    @classmethod
+    def sum(cls, v1, v2):
+        x = v1.x + v2.x
+        y = v1.y + v2.y
+        return cls(x,y)
+
+    @classmethod
+    def substract(cls, v1, v2):
+        x = v1.x - v2.x
+        y = v1.y - v2.y
+        return cls(x,y)
+    
+    @staticmethod
+    def dot_product(v1, v2):
+        return (v1.x*v2.x) + (v1.y*v2.y)
+    
+    @staticmethod
+    def distance(v1, v2):
+        return math.sqrt(((v1.x-v2.x)*(v1.x-v2.x))  + ((v1.y-v2.y)*(v1.y-v2.y)))
+    
+class Vector3D(Vector2D):
+
+    def __init__(self,x,y,z):
+        super().__init__(x,y)
+        self.z = z
+    
+    def __str__(self):
+        return "({},{},{})".format(self.x, self.y, self.z)
+
+    @property
+    def module(self):
+        square = math.sqrt((self.x*self.x) + (self.y*self.y) + (self.z*self.z))
+        square = round(square, 3)
+        return square
+
+    def scalar_prod(self, n=1):
+        x,y,z = n*self.x, n*self.y, n*self.z
+        return x,y,z
+    
+    @classmethod
+    def sum(cls, v1, v2):
+        x = v1.x + v2.x
+        y = v1.y + v2.y
+        z = v1.z + v2.z
+        return cls(x,y,z)
+    
+    @classmethod
+    def substract(cls, v1, v2):
+        x = v1.x - v2.x
+        y = v1.y - v2.y
+        z = v1.z - v2.z
+        return cls(x,y,z)
+
+    @staticmethod
+    def dot_product(v1, v2):
+        return (v1.x*v2.x) + (v1.y*v2.y) + (v1.z*v2.z)
+    
+
+    @staticmethod
+    def distance(v1, v2):
+        return math.sqrt(((v1.x-v2.x)*(v1.x-v2.x)) + ((v1.y-v2.y)*(v1.y-v2.y)) + ((v1.z-v2.z)*(v1.z-v2.z)))
+
+    
+        
+    
+    
+
+
